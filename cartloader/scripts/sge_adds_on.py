74c2cce63c6042c98d462c254f7f4a758dfbd395
Weiqiu Cheng <weiqiuc@gl-login2.arc-ts.umich.edu>
2025-03-07 22:03:51 -0500

    updated sge_adds_on

--- cartloader/scripts/sge_adds_on.py
+++ cartloader/scripts/sge_adds_on.py
@@ -1,6 +1,12 @@
 import sys, os, gzip, argparse, logging, warnings, shutil, re, copy, time, pickle, inspect, warnings, json, yaml
 import pandas as pd
+"""
+cartloader sge_adds_on \
+    --in-transcript /nfs/turbo/umms-leeju/nova/v2/analysis/n14-hm2tk-t07a-mouse-1bbd0/n14-hm2tk-t07a-mouse-1bbd0-mask-a3207/preprocess/n14-hm2tk-t07a-mouse-1bbd0-mask-a3207.transcripts.tsv.gz \
+    --out-feature /nfs/turbo/umms-leeju/nova/v2/analysis/n14-hm2tk-t07a-mouse-1bbd0/n14-hm2tk-t07a-mouse-1bbd0-mask-a3207/preprocess/n14-hm2tk-t07a-mouse-1bbd0-mask-a3207.feature.tsv.gz \
+    --colname-feature-id gene_id --add-feature
 
+"""
 
 def parse_arguments(_args):
     """Parse command-line arguments."""
@@ -9,22 +15,18 @@ def parse_arguments(_args):
     parser.add_argument("--in-transcript", type=str, help="Input file.", required=True)
     parser.add_argument("--add-feature", action='store_true', help="Add feature to the input file.")
     parser.add_argument("--add-minmax", action='store_true', help="Add minmax to the input file.")
-
     # feature args
     parser.add_argument("--out-feature", type=str, help="Output file for feature.", default=None)
     parser.add_argument('--colname-feature-name', type=str, default='gene', help='Feature name column (default: gene)')
     parser.add_argument('--colname-feature-id', type=str, default=None, help='Feature ID column (default: None)')
     parser.add_argument("--colnames-count", type=str, default="gn,gt,spl,unspl,ambig", help="Comma-separated column names for count (default: gn,gt,spl,unspl,ambig)")
-
     # minmax args
     parser.add_argument("--out-minmax", type=str, help="Output file for minmax.", default=None)
     parser.add_argument("--mu-scale", type=float, help="Scale factor for X and Y coordinates.", default=1)
     parser.add_argument("--minmax-format", type=str, help="Type of minmax to compute.", default="col")
-
     if len(_args) == 0:
         parser.print_help()
         sys.exit(1)
-
     return parser.parse_args(_args)
 
 def sge_add_feature(args):
