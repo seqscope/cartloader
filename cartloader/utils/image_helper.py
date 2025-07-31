297dbfa5a32eb7dc811b4c1c2acec2d011bbeabd
Weiqiu Cheng <weiqiuc@gl-login3.arc-ts.umich.edu>
2025-07-31 10:17:15 -0400

    debug: text in image_helper.py

--- cartloader/utils/image_helper.py
+++ cartloader/utils/image_helper.py
@@ -79,7 +79,7 @@ def update_orient(rotation, flip_vertical, flip_horizontal, image_f=None):
         if isinstance(rotation, int):
             rotation = str(rotation)
             if rotation not in ["90", "180", "270"]:
-                raise ValueError(f"Error: Invalid rotate value ({rotation}){" for"+image_f if image_f else ""}. Rotation must be None, 90, 180, or 270.")
+                raise ValueError(f"Error: Invalid rotate value ({rotation}){'for'+image_f if image_f else ''}. Rotation must be None, 90, 180, or 270.")
 
     new_rot, new_vflip, new_hflip = orient_map.get(
         (rotation, flip_vertical, flip_horizontal)
