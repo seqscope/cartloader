727ad79d1abf4c6bfdc9600d9477861608f25233
Weiqiu Cheng <wqcheng2010@gmail.com>
2026-03-03 13:36:24 -0500

    Updated the html prefix in run_ficture2

--- cartloader/scripts/run_ficture2.py
+++ cartloader/scripts/run_ficture2.py
@@ -405,7 +405,7 @@ def run_ficture2(_args):
                     f"--output_pref '{model_prefix}.factor'"
                     ])
                 cmds.append(cmd)
-                cmds.append(f"[ -f {lda_de} ] && [ -f {model_prefix}.factor.html ] && touch {model_prefix}_summary.done")
+                cmds.append(f"[ -f {lda_de} ] && [ -f {model_prefix}.factor.info.html ] && touch {model_prefix}_summary.done")
                 mm.add_target(f"{model_prefix}_summary.done", [f"{model_prefix}.done", color_map], cmds)
 
             # - umap
@@ -445,7 +445,7 @@ def run_ficture2(_args):
             decode_flag = f"{decode_prefix}.done"
 
             decode_de = f"{decode_prefix}.bulk_chisq.tsv"
-            decode_report = f"{decode_prefix}.factor.html"
+            decode_report = f"{decode_prefix}.factor.info.html"
             decode_summary_flag=f"{decode_prefix}_summary.done"
 
             #1) transform/fit
